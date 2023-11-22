33411e16935d50c3f88f41e07f629dc8b89170e5
Oscar <nombrede79@gmail.com>
2023-11-22 15:45:59 -0600

    nose
    smn

--- SysGymT.AccesoADatos/obj/Debug/net6.0/SysGymT.AccesoADatos.AssemblyInfo.cs
+++ SysGymT.AccesoADatos/obj/Debug/net6.0/SysGymT.AccesoADatos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SysGymT.AccesoADatos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72d7132daded27d20e273f2896a411ca11802586")]
 [assembly: System.Reflection.AssemblyProductAttribute("SysGymT.AccesoADatos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SysGymT.AccesoADatos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- SysGymT.LogicaDeNegocio/obj/Debug/net6.0/SysGymT.LogicaDeNegocio.AssemblyInfo.cs
+++ SysGymT.LogicaDeNegocio/obj/Debug/net6.0/SysGymT.LogicaDeNegocio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SysGymT.LogicaDeNegocio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72d7132daded27d20e273f2896a411ca11802586")]
 [assembly: System.Reflection.AssemblyProductAttribute("SysGymT.LogicaDeNegocio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SysGymT.LogicaDeNegocio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
